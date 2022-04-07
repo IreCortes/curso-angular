75c5a9bc6ad5b25a6fba56215d24aae119d7f418
IreCortes <irenegabardacortes@gmail.com>
2022-04-07 10:22:09 +0200

    estoy harta

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DbzModule } from './dbz/dbz.module';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SidebarComponent],
   imports: [BrowserModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
