9c9884b0bca00a82e08e01fa7eddb01efc3e6c81
IreCortes <irenegabardacortes@gmail.com>
2022-04-13 10:20:34 +0200

    Diversos cambios

--- heroesApp/src/app/heroes/heroes.module.ts
+++ heroesApp/src/app/heroes/heroes.module.ts
@@ -5,8 +5,9 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-
-
+import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [
@@ -14,10 +15,13 @@ import { ListadoComponent } from './pages/listado/listado.component';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    HeroesRoutingModule,
+    FlexLayoutModule,
+    MaterialModule,
+  ],
 })
-export class HeroesModule { }
+export class HeroesModule {}
